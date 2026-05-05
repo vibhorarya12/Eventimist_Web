06daf4bcbae2c70fbb787a8f9e7503bff275b6c0
Vibhor Arya <135149764+vibhorarya12@users.noreply.github.com>
2026-05-05 16:36:07 +0530

    add userregister service

---
+++ src/services/eventimist/user/auth/userRegister.service.ts
@@ -0,0 +1,32 @@
+
+import eventimistClient from "@/services/eventimist/client";
+
+export interface UserRegisterRequest {
+  name:           string;
+  email:          string;
+  password:       string;
+  clerkSessionId: string;
+}
+
+export interface UserRegisterResponse {
+  name:       string;
+  email:      string;
+  token:      string;
+  profilePic: string | null;
+}
+
+export interface UserRegisterError {
+  statusCode: number;
+  message:    string;
+  timestamp:  string;
+}
+
+export async function userRegister(
+  body: UserRegisterRequest
+): Promise<UserRegisterResponse> {
+  const res = await eventimistClient.post<UserRegisterResponse>(
+    "/auth/user/register",
+    body
+  );
+  return res.data;
+}
